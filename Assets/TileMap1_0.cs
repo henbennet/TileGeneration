9fa7692a23b598bcec413c2c4d54f2ddfa9eafe3
warj <henbennet@gmail.com>
2014-12-04 22:35:56 +0100

    Texture generation
    Generating texture
    Random generation based on texture mapping

--- Assets/TileMap1_0.cs
+++ Assets/TileMap1_0.cs
@@ -7,68 +7,37 @@ using System.Collections;
 [RequireComponent(typeof(MeshRenderer))] 
 [RequireComponent(typeof(MeshCollider))] 
 
-public class TileMap : MonoBehaviour {
-
+public class TileMap1_0 : MonoBehaviour {
+	
 	//Number of tiles
 	public int size_x = 100;
 	public int size_z = 50;
 	public float tileSize = 1.0f;
-
-	public Texture2D terrainTiles;
-	public int tileResolution;
-
 	// Use this for initialization
 	void Start () {
 		BuildMesh ();
 	}
-
-
-
-	void buildTexture() {
-		tileResolution = terrainTiles.height;
-		int numTilesPerRow = terrainTiles.width / tileResolution;
-		int numRows = terrainTiles.height / tileResolution;
-
-		int textWith = size_x * tileResolution;
-		int textHeight = size_z * tileResolution;
-		Texture2D texture = new Texture2D (textWith, textHeight);
-
-		for (int y=0; y<size_z; y++) {
-			for (int x=0; x<size_x; x++) {
-				Color[] p = terrainTiles.GetPixels(Random.Range(0,4)*tileResolution, 0, tileResolution, tileResolution);
-				texture.SetPixels(x * tileResolution, y * tileResolution, tileResolution, tileResolution, p);
-
-			}
-		}
-		texture.filterMode = FilterMode.Point;
-		texture.Apply ();
-
-		MeshRenderer mr = GetComponent<MeshRenderer> ();
-		mr.sharedMaterials [0].mainTexture = texture;
-
-		Debug.Log ("Texture built");
-	}
-
+	
 	public void BuildMesh() {
-
+		
 		int numberOfTiles = size_x * size_z;
 		int numTris = numberOfTiles * 2;
 		
 		int vsize_x = size_x + 1;
 		int vsize_z = size_z + 1;
 		int numVerts = vsize_x * vsize_z;
-
-
+		
+		
 		//Generate mesh data
 		Vector3[] vertices = new Vector3[numVerts];
 		Vector3[] normals = new Vector3[numVerts];
 		Vector2[] uv = new Vector2[numVerts];
 		int[] triangles = new int[numTris*3];
-
+		
 		int z, x;
 		for (z=0; z<size_z; z++) {
 			for (x=0; x<size_x; x++) {
-				vertices[z * vsize_x + x] = new Vector3(x * tileSize, 0 /*Random.Range(-1f, 1f)*/, z * tileSize);
+				vertices[z * vsize_x + x] = new Vector3(x * tileSize, Random.Range(-1f, 1f), z * tileSize);
 				normals[z * vsize_x + x] = Vector3.up;
 				uv[ z * vsize_x + x ] = new Vector2( (float)x / vsize_x, (float)z / vsize_z );
 			}
@@ -91,7 +60,7 @@ public class TileMap : MonoBehaviour {
 		}
 		
 		Debug.Log ("Done Triangles!");
-
+		
 		//Create a new mesh and populate it
 		Mesh mesh = new Mesh ();
 		mesh.vertices = vertices;
@@ -107,56 +76,55 @@ public class TileMap : MonoBehaviour {
 		mf.mesh = mesh;
 		mc.sharedMesh = mesh;
 		Debug.Log ("Done Mesh!");
-
-		buildTexture ();
+		
 	}
-
-
+	
+	
 	void builSingledMesh() {
 		//Generate mesh data
 		Vector3[] vertices = new Vector3[4];
 		int[] triangles = new int[6];
 		Vector3[] normals = new Vector3[4];
 		Vector2[] uv = new Vector2[4];
-
-
+		
+		
 		vertices [0] = new Vector3 (0, 0, 0);
 		vertices [1] = new Vector3(1, 0, 0);
 		vertices [2] = new Vector3 (0, 0, -1);
 		vertices [3] = new Vector3 (1, 0, -1);
-
+		
 		triangles [0] = 0;
 		triangles [1] = 3;
 		triangles [2] = 2;
 		triangles [3] = 0;
 		triangles [4] = 1;
 		triangles [5] = 3;
-
+		
 		normals [0] = Vector3.up;
 		normals [1] = Vector3.up;
 		normals [2] = Vector3.up;
 		normals [3] = Vector3.up;
-				
+		
 		uv [0] = new Vector3 (0, 1);
 		uv [1] = new Vector3 (1, 1);
 		uv [2] = new Vector3 (0, 0);
 		uv [3] = new Vector3 (1, 0);
-
+		
 		//Create a new mesh and populate it
 		Mesh mesh = new Mesh ();
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.normals = normals;
 		mesh.uv = uv;
-
+		
 		//Assign mesh to filter/renderer/collider
 		MeshFilter mf = GetComponent<MeshFilter> ();
 		MeshRenderer mr = GetComponent<MeshRenderer> ();
 		MeshCollider mc = GetComponent<MeshCollider> ();
 		mf.mesh = mesh;
-
+		
 	}
-
-
-
+	
+	
+	
 }
---
+++ Assets/TileMapMouse.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TileMap))]
+public class TileMapMouse : MonoBehaviour {
+	public Color normalColor = Color.red;
+	public Color overColor = Color.green;
+	public Transform selectionCube;
+
+
+	TileMap tileMap;
+	Vector3 currentTileCoord;
+
+	// Use this for initialization
+	void Start () {
+		tileMap = GetComponent<TileMap>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Ray ray = new Ray (camera.transform.position, camera.transform.forward);	
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitinfo;
+		
+		if (collider.Raycast(ray, out hitinfo, Mathf.Infinity)) { 
+			Debug.Log (/*transform.worldToLocalMatrix * */hitinfo.point - transform.position);
+			int x = Mathf.FloorToInt(hitinfo.point.x / tileMap.tileSize);
+			int z = Mathf.FloorToInt(hitinfo.point.z / tileMap.tileSize);
+
+			Debug.Log("Tile: " + x + ", " + z);
+			currentTileCoord.x = x;
+			currentTileCoord.z = z;
+			selectionCube.transform.position = currentTileCoord * tileMap.tileSize;
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+
+		}
+
+	}
+}
