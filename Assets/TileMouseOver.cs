7b8173d6b097612fd62d481b01e8b9b5d77f316c
warj <henbennet@gmail.com>
2014-12-04 20:40:58 +0100

    Test TileGenerate 1.0

---
+++ Assets/TileMouseOver.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMouseOver : MonoBehaviour {
+
+	public Color normalColor;
+	public Color overColor = Color.green;
+
+	void Start() {
+		normalColor = renderer.material.color;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Ray ray = new Ray (camera.transform.position, camera.transform.forward);	
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitinfo;
+
+		if (collider.Raycast(ray, out hitinfo, Mathf.Infinity)) { 
+						renderer.material.color = overColor;
+		} else {
+			renderer.material.color = normalColor;
+		}
+
+	}
+
+	void OnMouseOver() {
+		renderer.material.color = overColor;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = normalColor;
+	}
+
+}
